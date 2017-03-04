1889c1ec0c9e3135e4010ef7d07f661e7ef9b1f8
homelleon <ihomelleoni@yandex.ru>
2017-03-05 00:54:08 +0300

    [added: 02] image, style

--- app/js/javaScript.js
+++ app/js/javaScript.js
@@ -2,20 +2,55 @@ window.onload = function () {
 	var button = document.getElementsByClassName('button');
 	
 	for(var i = 0; i < button.length; i++) {
-		b = button[i];
+		var b = button[i];
+		
 		b.onmouseover = function() {
+			this.style.color = "red";
 			this.style.backgroundColor = "blue";
 		};
 		
 		b.onmouseout = function() {
-			this.style.color = "red";
+			this.style.color = "";
 			this.style.backgroundColor = "";
 		};
 		
 		b.onclick = function() {
-			alert('Pressed!');
-			this.style.backgroundColor = "";
+			this.style.backgroundColor = "rgb(0,0,125)";
+			this.style.color = "rgb(10,0,0)";
+			var self = this;
+			function returnBack() {
+				self.style.color = "red";
+				self.style.backgroundColor = "blue";
+			}
+			setTimeout(returnBack, 100);
 		};
+	}
+	
+	var linkFooter = document.getElementsByClassName('footer')[0].getElementsByTagName('a');
+	
+	for(var i = 0; i < linkFooter.length; i++) {
+		var l = linkFooter[i];
+		
+		l.onmouseover = function() {
+			this.style.color = "white";
+			this.style.border = "2px solid white";
+		}
+		
+		l.onmouseout = function() {
+			this.style.color = "";
+			this.style.border = "";
+		}
+		
+		l.onclick = function() {
+			this.style.color = "blue";
+			this.style.border = "2px solid blue";
+			var self = this;
+			function returnBack() {
+				self.style.color = "white";
+				self.style.border = "2px solid white";
+			}
+			setTimeout(returnBack, 100);
+		}
 	}
 }
 
